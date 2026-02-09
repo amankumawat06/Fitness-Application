57c6f859cac0c029dd613da7186771f65386334f
Aman kumawat <amankumawat4507@gmail.com>
2026-02-09 16:30:29 +0500

    Added forntend path to cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors({
     origin: [
-        "http://localhost:5173",
+        "https://fit-track-fitness-application.vercel.app"
     ],
     credentials:true,
 }))
